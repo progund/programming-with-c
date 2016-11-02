b34ce6e4c987d25c9b874afba18097d074a93b79
Henrik Sandklef <hesa@sandklef.com>
2016-11-02 15:58:40 +0100

    Asswipe henrik fixed err

--- variables-and-types/limit.c
+++ variables-and-types/limit.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <limits.h>
 
+/* NOT WORKIN on all platforms :(
 #define PRINT_TYPE(t, min, max) \
   printf("%.20s\n=======================\n"  \
          " size:     %d byte(s)\n\n"         \
@@ -28,8 +29,6 @@
          sizeof(t),                          \
          min, max,                           \
          min, max);
-
-int  main() {
   PRINT_TYPE(char,           CHAR_MIN, CHAR_MAX);
   PRINT_UTYPE(unsigned char, 0,        UCHAR_MAX);
   PRINT_TYPE(short,          SHRT_MIN, SHRT_MAX);
@@ -38,6 +37,17 @@ int  main() {
   PRINT_UTYPE(unsigned int,  0,        UINT_MAX);
   PRINT_TYPE(long,           LONG_MIN, LONG_MAX);
   PRINT_UTYPE(unsigned long, 0,        ULONG_MAX);
+*/
+
+#define PRINT_STR_AND_SIZE(a) printf("\n%.20s (%d bytes)\n------------------\n", #a, sizeof(a))
+
+int  main() {
+  PRINT_STR_AND_SIZE(unsigned char);
+  printf (" min: %d %x\n", 0, 0);
+  printf (" max: %d %x\n", UCHAR_MAX, UCHAR_MAX);
+  PRINT_STR_AND_SIZE(char);
+  printf (" min: %d %x\n", CHAR_MIN, CHAR_MIN);
+  printf (" max: %d %x\n", CHAR_MAX, CHAR_MAX);
   return 0;
 }
 
