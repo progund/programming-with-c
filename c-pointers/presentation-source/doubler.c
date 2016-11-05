b14058e75d24d91e48837d6e4e6b15fbc72bfa8a
Henrik Sandklef <hesa@sandklef.com>
2016-11-05 23:48:16 +0100

    more comments

--- c-pointers/presentation-source/doubler.c
+++ c-pointers/presentation-source/doubler.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+/*
+ *
+ * Simple program with a function returning the value of an integer
+ * passed to it, multiplied by two
+ *
+ */ 
 int doubler(int x) {
+  // x is an int, with a value
+  // multiply the value of x with two, and then return the product
   return x * 2;
 }
 
@@ -8,7 +16,10 @@ int main() {
   int orig = 12;
 
   printf ("orig: %d\n", orig);
-  orig  = doubler(12);
+  // Pass the orig variable (with the value 12) to doubler Since
+  // doubler does npt update the variable (try for yourself) we need
+  // to store the value returned by doubler in the orig variable.
+  orig  = doubler(orig);
   printf ("orig: %d\n", orig);
 
   return 0;
