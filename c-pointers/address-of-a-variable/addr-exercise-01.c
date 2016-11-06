acdd3a6aff2938e2b5689a689484e102aeba2419
Henrik Sandklef <hesa@sandklef.com>
2016-11-06 17:21:32 +0100

    First rev

---
+++ c-pointers/address-of-a-variable/addr-exercise-01.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ *
+ * Small program to show how to get the address of a variable
+ *
+ */
+int main()
+{
+  /* declare an int variable - no assignment */
+  int x;
+  /* print the value */
+  printf ("x value:     %d\n", x);
+  /* print the address 
+     we cast since "%p"expects a void* instead of the int* we supply
+  */
+  printf ("x address:   %p\n", (void*)&x);
+
+  
+  return 0;
+}
