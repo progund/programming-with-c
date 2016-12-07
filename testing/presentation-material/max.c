01ae77f2ae249afcc1063130ab69a9faca0fc688
Henrik Sandklef <hesa@sandklef.com>
2016-12-07 08:53:17 +0100

    First rev

---
+++ testing/presentation-material/max.c
@@ -0,0 +1,11 @@
+#include "max.h"
+
+int
+max(int a, int b)
+{
+  if (a>b)
+    {
+      return a;
+    }
+  return b;
+}
---
+++ testing/presentation-material/max.h
@@ -0,0 +1,7 @@
+#ifndef MAX_H
+#define MAX_H
+
+int
+max(int a, int b);
+
+#endif /* MAX_H */
