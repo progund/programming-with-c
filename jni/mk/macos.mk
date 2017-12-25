b8db3e077cc60f838f38d9a4dc4b737d6159d3c9
Henrik Sandklef <hesa@sandklef.com>
2017-12-25 19:49:31 +0100

    macos settings

---
+++ jni/mk/macos.mk
@@ -0,0 +1,6 @@
+JNI_PATH=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.7.sdk/System/Library/Frameworks/JavaVM.fra\
+mework/Versions/A/Headers/
+JNI_INCS=-I$(JNI_PATH)/
+JNI_LD_FLAGS=-dynamiclib -fPIC
+JNI_LIB_NAME=liblove.jnilib
+
