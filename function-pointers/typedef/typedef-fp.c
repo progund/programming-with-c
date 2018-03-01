86bd4e7bcb36adbadd3bf3d7e2eb92171d4ebbba
Henrik Sandklef <hesa@sandklef.com>
2018-03-01 22:52:32 +0100

    first rev

---
+++ function-pointers/typedef/typedef-fp.c
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2018, Henrik Sandklef, Rikard Fröberg
+ *
+ */
+#include <stdio.h>
+#include "typedef-fp.h"
+
+/*
+ * Global varible poiting to th feedback function choosen
+ *
+ */
+feedback_ptr feedback_fun ;
+
+
+/*
+ * Name:         default_feedback
+ * Args:         str - string to print
+ * Description:  Prints a string to given file
+ */
+int
+default_feedback(const char *str)
+{
+  return fprintf(stderr, "[%s] %s", __func__, str);
+}
+
+
+
+int main(void)
+{
+  /* Set feedback_fun to point at default feedback function */
+  feedback_fun = default_feedback ;
+
+  /* Call the  default_feedback in the ordinary way */
+  printf ("Calling the function the usual way\n");
+  default_feedback("Ordinary call\n");
+
+  /* Call the default_feedback through the feedback_fun variable */
+  printf ("Calling the function via the function pointer\n");
+  feedback_fun("Called using function pointer call\n");
+  return 0;
+}
---
+++ function-pointers/typedef/typedef-fp.h
@@ -0,0 +1,6 @@
+#ifndef TYPEDEF_FP_H
+#define TYPEDEF_FP_H
+
+typedef int (*feedback_ptr)(const char *str);
+
+#endif /* TYPEDEF_FP_H */
