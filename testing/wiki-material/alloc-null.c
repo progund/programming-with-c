58ef0ccbd23ed8b5f47aabdfe426793a15706d33
Henrik Sandklef <hesa@sandklef.com>
2018-03-14 00:03:29 +0100

    first rev

---
+++ testing/wiki-material/alloc-null.c
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+
+void *malloc(size_t size) { return NULL; } 
+void free(void *ptr) { return ;} 
+void *calloc(size_t nmemb, size_t size) { return NULL; } 
+void *realloc(void *ptr, size_t size)  { return NULL; } 
