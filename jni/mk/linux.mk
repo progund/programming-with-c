084fd4541e6f12d3b762ea60c3148cb9cdb3000f
Henrik Sandklef <hesa@sandklef.com>
2018-12-08 22:16:17 +0100

    generic mk for linux

--- jni/mk/linux.mk
+++ jni/mk/linux.mk
@@ -1,4 +1,5 @@
-JNI_PATH=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.151-1.b12.fc27.x86_64
-JNI_INCS=-I$(JNI_PATH)/include -I$(JNI_PATH)/include/linux
+JNI_H=$(shell find /usr/lib/jvm -name "jni.h" | tail -1)
+JNI_PATH=$(shell dirname $(JNI_H))
+JNI_INCS=-I$(JNI_PATH) -I$(JNI_PATH)/linux
 JNI_LD_FLAGS=-shared -fPIC
 JNI_LIB_NAME=liblove.so
