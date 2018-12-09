722e4564a2016fd8bb6a6739798b684e5e71f4d4
Henrik Sandklef <hesa@sandklef.com>
2018-12-09 12:43:27 +0100

    shell make

--- jni/mk/macos.mk
+++ jni/mk/macos.mk
@@ -1,6 +1,6 @@
 
-JNI_H=`find /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacO*/System/Library/Frameworks/JavaVM.framework/Versions/A/Headers/jni.h | tail -1)`
-JNI_PATH=`dirname JNI_H`
+JNI_H="$(shell find /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacO*/System/Library/Frameworks/JavaVM.framework/Versions/A/Headers/jni.h | tail -1)"
+JNI_PATH="$(shell dirname $(JNI_H))"
 JNI_INCS=-I$(JNI_PATH)/
 JNI_LD_FLAGS=-dynamiclib -fPIC
 JNI_LIB_NAME=liblove.jnilib
