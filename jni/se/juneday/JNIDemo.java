a41a6fc97ca8330a5cd582abe2b489b92c50e3d7
Henrik Sandklef <hesa@sandklef.com>
2018-12-09 12:18:00 +0100

    test code (uncomment to use)

--- jni/se/juneday/JNIDemo.java
+++ jni/se/juneday/JNIDemo.java
@@ -5,11 +5,12 @@ public class JNIDemo {
   public static void main(String[] args){
 
     FromCWithLove c = new FromCWithLove();
-
-    String msg = c.getSome("Krega");
-
-    System.out.println("In Java again: " + msg);
     
+    //    for (int i=0 ; i <10_000_000; i++) {
+    //  System.out.print(" * " + i + ": ");
+    String msg = c.getSome("Krega");
+    System.out.println("In Java again: " + msg);
+    //}
   }
   
 }
