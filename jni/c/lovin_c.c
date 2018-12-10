20f2f681acacfa40acf300f62b083deb89584be2
Henrik Sandklef <hesa@sandklef.com>
2018-12-10 01:14:52 +0100

    more printouts

--- jni/c/lovin_c.c
+++ jni/c/lovin_c.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define LOG(a) fprintf(stderr, "[%s:%d] C code got \"%s\"\n", __FILE__, __LINE__, a);
+#define LOG(a)  fprintf(stderr, "[%s:%d] %s\n", __FILE__, __LINE__, a);
 const static char* C_MSG_START = "From C code with love, we got \"";
-const static char* C_MSG_END = "\"";
+const static char* C_MSG_END = "\" from Java.";
 
 JNIEXPORT jstring JNICALL Java_se_juneday_FromCWithLove_getSome
   (JNIEnv *env, jobject obj, jstring msg)
@@ -13,10 +13,12 @@ JNIEXPORT jstring JNICALL Java_se_juneday_FromCWithLove_getSome
   jstring ret_jstring;
   const char *from_java =
     (*env)->GetStringUTFChars(env, msg, NULL);
+  LOG(" ----- Entering C code ------");
   if (from_java==NULL)
     {
       return NULL;
     }
+  LOG("C code got this from Java: ");
   LOG(from_java);
 
   char * ret_string = calloc(sizeof(char),
@@ -35,12 +37,19 @@ JNIEXPORT jstring JNICALL Java_se_juneday_FromCWithLove_getSome
   strcat(ret_string, from_java);
   strcat(ret_string, C_MSG_END);
 
+  // Tell JVM we're not interested in msg anymore
+  (*env)->ReleaseStringUTFChars(env, msg, from_java);
+
   // convert to Jav String
   ret_jstring = (*env)->NewStringUTF(env, ret_string);
 
+  LOG("The following string will be returned back to Java:" );
+  LOG(ret_string);
+  
   // free (de-allocate) memory of C string
   free (ret_string);
   
+  LOG(" ----- Leaving C code ------");
   return ret_jstring;
 }
 
